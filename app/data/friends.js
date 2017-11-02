a851e8e59e9560d1f30384c24f02cc05b0ed2dff
Bryce Rothhammer <rothhammer.b@gmail.com>
2017-11-02 12:03:30 -0500

    create friends array and export it

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var friends = [
+  {
+    name: "Bob",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    scores: [
+      "3",
+      "2",
+      "3",
+      "5",
+      "4",
+      "1",
+      "3",
+      "5",
+      "5",
+      "1"
+    ]
+  },
+  {
+    name: "Josh",
+    photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "2",
+      "2",
+      "2",
+      "2",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Kate",
+    photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    scores: [
+      "1",
+      "1",
+      "4",
+      "4",
+      "4",
+      "5",
+      "3",
+      "3",
+      "1",
+      "5"
+    ]
+  },
+  {
+    name: "Steve",
+    photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    scores: [
+      "2",
+      "2",
+      "5",
+      "1",
+      "3",
+      "1",
+      "5",
+      "1",
+      "2",
+      "1"
+    ]
+  },
+  {
+    name: "Ashley",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+    scores: [
+      "2",
+      "1",
+      "1",
+      "1",
+      "4",
+      "5",
+      "5",
+      "1",
+      "3",
+      "2"
+    ]
+  }
+
+];
+
+// export array for other files to access
+module.exports = friends;
