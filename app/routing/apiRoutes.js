8c3fcac945364a446b8b2baf0c744def67ef4d40
Bryce Rothhammer <rothhammer.b@gmail.com>
2017-11-02 21:39:09 -0500

    heroku setup

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,32 @@
+var friends = require("../data/friends");
+
+module.exports = function(app){
+
+	//when user goes to a page
+	app.get("/api/friends", function(request, response){
+		response.json(friends);
+	});
+
+	//when user submits the form
+	app.post("/api/friends", function(request, response){
+		
+
+		var userAnswers = req.body;
+		var userScores = userData.scores;
+		var compareFriends;
+
+		var match = {
+			name: "",
+			photo: "",
+			friendMatch: Infinity
+		};
+
+		for (var i = 0; i < friends.length; i++){
+			var 
+		}
+
+	});
+
+
+
+}
